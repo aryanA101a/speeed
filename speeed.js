930bc95cf30d3de5375b49f49f28f93153d30eeb
Aryan Arora <aryanarora.w1@gmail.com>
2022-12-07 22:21:08 +0530

    refactoring

--- speeed.js
+++ speeed.js
@@ -1,13 +1,13 @@
+var video = document.getElementsByTagName("video")[0];
 chrome.runtime.onMessage.addListener((message, sender, response) => {
   const { command } = message;
-    
 
   if (command == "increase_speeed") {
-    document.getElementsByTagName("video")[0].playbackRate += 0.25;
-    console.log(document.getElementsByTagName("video")[0].playbackRate);
-  } else if (command == "decrease_speeed") {
-    document.getElementsByTagName("video")[0].playbackRate -= 0.25;
-    console.log(document.getElementsByTagName("video")[0].playbackRate);
+    video.playbackRate += 0.25;
+    console.log(video.playbackRate);
+  } else if (command == "decrease_speeed" && video.playbackRate != 0) {
+    video.playbackRate -= 0.25;
+    console.log(video.playbackRate);
   }
   return true;
 });
